c8a1c325ee887f977ba1694a264ad95bfe43bc42
jackpan <826647196@qq.com>
2020-12-17 23:53:18 +0800

    Delegating.

---
+++ src/main/java/com/jackpan/concurrent/DelegatingVehicleTracker.java
@@ -0,0 +1,46 @@
+package com.jackpan.concurrent;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author jackpan
+ * @version v1.0 2020/12/17 23:23
+ */
+public class DelegatingVehicleTracker {
+
+    private final ConcurrentHashMap<String, Point> locations;
+
+    private final Map<String, Point> unmodifiableMap;
+
+    public DelegatingVehicleTracker(Map<String, Point> points) {
+        locations = new ConcurrentHashMap<>(points);
+        unmodifiableMap = Collections.unmodifiableMap(this.locations);
+    }
+
+
+    /**
+     * Gets locations.
+     *
+     * @return Value of locations.
+     */
+    public Map<String, Point> getLocations() {
+        return this.unmodifiableMap;
+    }
+
+    /**
+     * Gets unmodifiableMap.
+     *
+     * @return Value of unmodifiableMap.
+     */
+    public Point getLocation(String id) {
+        return this.locations.get(id);
+    }
+
+    public void setLocation(String id, int x, int y) {
+        if (this.locations.replace(id, new Point(x, y)) == null) {
+            throw new IllegalArgumentException("invalid vehicle name: " + id);
+        }
+    }
+}
---
+++ src/main/java/com/jackpan/concurrent/Point.java
@@ -0,0 +1,15 @@
+package com.jackpan.concurrent;
+
+/**
+ * @author jackpan
+ * @version v1.0 2020/12/17 23:22
+ */
+public class Point {
+
+    public final int x, y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}

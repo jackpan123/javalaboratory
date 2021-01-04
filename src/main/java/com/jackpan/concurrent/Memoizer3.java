90fd1c2b171bf7a67ec39bcf1a8e14b62b2d6ad7
jackpan <826647196@qq.com>
2021-01-04 22:59:24 +0800

    Memoizer three.

---
+++ src/main/java/com/jackpan/concurrent/Memoizer3.java
@@ -0,0 +1,43 @@
+package com.jackpan.concurrent;
+
+import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
+/**
+ * @author jackpan
+ * @version v1.0 2021/1/4 22:39
+ */
+public class Memoizer3<A, V> implements Computable<A, V> {
+
+    private final Map<A, FutureTask<V>> cache = new ConcurrentHashMap<>();
+    private final Computable<A, V> c;
+
+    public Memoizer3(Computable<A, V> c) {
+        this.c = c;
+    }
+
+    public synchronized V compute(A arg) throws InterruptedException {
+        FutureTask<V> f = cache.get(arg);
+        if (f == null) {
+            Callable<V> eval = new Callable<V>() {
+                @Override
+                public V call() throws InterruptedException {
+                    return c.compute(arg);
+                }
+            };
+            FutureTask ft = new FutureTask<>(eval);
+            f = ft;
+            cache.put(arg, ft);
+            ft.run();
+        }
+        try {
+            return f.get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+            throw new InterruptedException(e.getMessage());
+        }
+    }
+}

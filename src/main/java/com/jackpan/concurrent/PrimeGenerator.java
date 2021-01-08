a9a5f38f96a64d821c3c55528983582a6f44c5c7
jackpan <826647196@qq.com>
2021-01-08 22:19:26 +0800

    Join task interrupt.

--- src/main/java/com/jackpan/concurrent/PrimeGenerator.java
+++ src/main/java/com/jackpan/concurrent/PrimeGenerator.java
@@ -3,6 +3,8 @@ package com.jackpan.concurrent;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -44,6 +46,42 @@ public class PrimeGenerator implements Runnable {
         }
 
         return generator.get();
+    }
+
+    private static final ScheduledExecutorService cancelExc =
+            new ScheduledThreadPoolExecutor(2);
+    public static void timeRun(final Runnable r,
+                               long timeout, TimeUnit unit)
+        throws InterruptedException {
+        class RethrowableTask implements Runnable {
+            private volatile Throwable t;
+            public void run() {
+                try {
+                    r.run();
+                } catch (Throwable t) {
+                    this.t = t;
+                }
+            }
+
+            void rethrow() {
+                if (t != null) {
+                    throw new NullPointerException();
+                }
+            }
+        }
+
+        RethrowableTask task = new RethrowableTask();
+        final Thread taskThread = new Thread(task);
+        taskThread.start();
+        cancelExc.schedule(new Runnable() {
+            @Override
+            public void run() {
+                taskThread.interrupt();
+            }
+        }, timeout, unit);
+
+        taskThread.join(unit.toMillis(timeout));
+        task.rethrow();
     }
 
     public static void main(String[] args) {
